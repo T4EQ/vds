1b4cc34ba6611af98471ff9543cd287cef99c2b0
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-07-27 14:39:15 +0200

    Serve content files from folder

--- vds-server/src/lib.rs
+++ vds-server/src/lib.rs
@@ -1,13 +1,15 @@
-use actix_web::{App, HttpServer, dev::Server};
+use actix_web::{App, HttpServer, dev::Server, web};
 
-use std::net::TcpListener;
+use std::{net::TcpListener, path::Path};
 
 mod api;
 mod static_files;
 
-pub fn run_app(listener: TcpListener) -> anyhow::Result<Server> {
+pub fn run_app(listener: TcpListener, content_path: &Path) -> anyhow::Result<Server> {
+    let content_path = web::Data::new(content_path.to_owned());
     Ok(HttpServer::new(move || {
         App::new()
+            .app_data(content_path.clone())
             .configure(api::register_handlers)
             .configure(static_files::register_static_files)
     })
--- vds-server/src/main.rs
+++ vds-server/src/main.rs
@@ -1,14 +1,17 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, path::PathBuf};
 
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(short, long, default_value = "0.0.0.0")]
+    #[arg(short, long, default_value = "127.0.0.1")]
     listen_address: String,
 
     #[arg(short, long, default_value = "8080")]
     port: u16,
+
+    #[arg(short, long)]
+    content_path: PathBuf,
 }
 
 #[actix_web::main]
@@ -16,6 +19,6 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let listener = TcpListener::bind(format!("{}:{}", args.listen_address, args.port))?;
     println!("Listening on {}", listener.local_addr()?);
-    vds_server::run_app(listener)?.await?;
+    vds_server::run_app(listener, &args.content_path)?.await?;
     Ok(())
 }
