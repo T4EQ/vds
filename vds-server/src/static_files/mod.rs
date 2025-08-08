52cf78e1d7ad2ed0986717286100f32bb879c452
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-08-08 20:16:09 +0200

    Clean cargo clippy

--- vds-server/src/static_files/mod.rs
+++ vds-server/src/static_files/mod.rs
@@ -1,9 +1,18 @@
 use actix_web::web;
 
-include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+mod static_files {
+    #![allow(
+        clippy::all,
+        clippy::pedantic,
+        clippy::nursery,
+        unused_imports,
+        unused_variables
+    )]
+    include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+}
 
 pub fn register_static_files(app: &mut web::ServiceConfig) {
-    let generated = generate();
+    let generated = static_files::generate();
     app.service(
         actix_web_static_files::ResourceFiles::new("/", generated).resolve_not_found_to_root(),
     );
