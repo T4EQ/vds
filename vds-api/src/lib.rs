bb7a12dba10bd8de35796619780c3cc0816f20cb
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-10-25 15:21:24 +0200

    Introduce new manifest file

--- vds-api/src/lib.rs
+++ vds-api/src/lib.rs
@@ -1,6 +1,3 @@
-// TODO: make this more organized instead of copying the data types.
-// TODO: add type safety
-
 //! The `vds-api` crate defines common data types shared by `vds-site` and `vds-server`.
 //!
 //! The create follows these conventions:
@@ -15,16 +12,12 @@
 //!
 //! The following terminology is used in the VDS:
 //! - `local` refers to content local to the VDS server.
-//! - `remote` refers to content in the remote servers (e.g.: S3, not in the LAN).
 //!
 //! The supported endpoints are:
-//!  - `GET` `api/content/remote`. Returns a list of the content in the remote server (e.g.: S3).
-//!  - `GET` `api/content/local`. Returns a list of the content in the local server (VDS).
-//!  - `DELETE` `api/content/local`. Deletes a piece of content from the local server.
-//!  - `PUT` `api/content/local`. Caches a piece of content from the remote server in the local
-//!    server.
-//!  - `GET` `api/content`. Obtains the requested content from the server. The Query requires an
-//!    resource ID
+//!  - `POST` `api/manifest/fetch`. Triggers an immediate fetch of the manifest, causing the VDS to
+//!    update its cached content.
+//!  - `GET` `api/manifest/latest`. Returns the latest manifest that is in use by the VDS.
+//!  - `GET` `api/content/meta`. Returns a list of the content in the local server (VDS).
 //!  - `GET` `api/content/{id}`. Obtains the requested content from the server. The path indicates
 //!    the resource ID.
 
@@ -32,57 +25,11 @@ mod types;
 
 pub mod api {
     pub mod content {
-        pub mod remote {
-            pub mod get {
-                pub use crate::types::RemoteVideoMeta;
-
-                /// The response to the `GET` `api/content/remote` request
-                #[derive(Default, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
-                pub struct Response {
-                    /// List of videos of the remote
-                    pub videos: Vec<RemoteVideoMeta>,
-                }
-
-                /// The query that can be used in a `GET` `api/content/remote` request
-                #[derive(Default, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
-                pub struct Query {
-                    /// Maximum number of videos to list
-                    pub limit: Option<usize>,
-                    // TODO: add pagination (offset).
-                }
-            }
-        }
-
-        pub mod local {
-            pub mod single {
-                pub mod get {
-                    pub use crate::types::{LocalVideoMeta, Progress, VideoStatus};
-
-                    /// The query that can be used in a `GET` `api/content/local/single` request
-                    #[derive(
-                        Default, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq,
-                    )]
-                    pub struct Query {
-                        /// Unique identifier of the video
-                        pub id: String,
-                        /// Maximum number of videos to list
-                        pub limit: Option<usize>,
-                        // TODO: add pagination (offset).
-                    }
-
-                    /// The response to the `GET` `api/content/local/single` request
-                    #[derive(Debug, serde::Deserialize, serde::Serialize, PartialEq)]
-                    pub struct Response {
-                        /// Video metadata, if found
-                        pub video: Option<LocalVideoMeta>,
-                    }
-                }
-            }
-
+        pub mod meta {
             pub mod get {
                 pub use crate::types::{LocalVideoMeta, Progress, VideoStatus};
 
-                /// The query that can be used in a `GET` `api/content/local` request
+                /// The query that can be used in a `GET` `api/content/meta` request
                 #[derive(Default, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
                 pub struct Query {
                     /// Maximum number of videos to list
@@ -90,39 +37,23 @@ pub mod api {
                     // TODO: add pagination (offset).
                 }
 
-                /// The response to the `GET` `api/content/local` request
+                /// The response to the `GET` `api/content/meta` request
                 #[derive(Debug, serde::Deserialize, serde::Serialize, PartialEq)]
                 pub struct Response {
-                    /// Locally cached content
                     pub videos: Vec<LocalVideoMeta>,
                 }
             }
 
-            pub mod delete {
-                /// The query that can be used in a `DELETE` `api/content/local` request
-                #[derive(Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
-                pub struct Query {
-                    /// Unique identifier of the video
-                    pub id: String,
-                }
-            }
+            pub mod single {
+                pub mod get {
+                    pub use crate::types::{LocalVideoMeta, Progress, VideoStatus};
 
-            pub mod put {
-                /// The query that can be used in a `PUT` `api/content/local` request
-                #[derive(Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
-                pub struct Query {
-                    /// Unique identifier of the video
-                    pub id: String,
+                    /// The response to the `GET` `api/content/meta/single/{id}` request
+                    #[derive(Debug, serde::Deserialize, serde::Serialize, PartialEq)]
+                    pub struct Response {
+                        pub meta: Option<LocalVideoMeta>,
+                    }
                 }
-            }
-        }
-
-        pub mod get {
-            /// The query that can be used in a `GET` `api/content` request
-            #[derive(Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
-            pub struct Query {
-                /// Unique identifier of the video
-                pub id: String,
             }
         }
     }
--- vds-server/src/api/user.rs
+++ vds-server/src/api/user.rs
@@ -1,6 +1,73 @@
-use actix_web::{HttpResponse, Responder, get, web};
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    sync::LazyLock,
+};
+
+use actix_web::{HttpResponse, Responder, get, post, web};
 use tokio::io::AsyncReadExt;
+
+use vds_api::api::content::meta::get::{LocalVideoMeta, Progress, VideoStatus};
+
+static MOCK_VIDEOS: LazyLock<Vec<LocalVideoMeta>> = LazyLock::new(|| {
+    vec![
+        LocalVideoMeta {
+            id: "1".to_string(),
+            name: "Introduction to Mathematics".to_string(),
+            size: 245 * 1024 * 1024,
+            status: VideoStatus::Downloaded,
+        },
+        LocalVideoMeta {
+            id: "2".to_string(),
+            name: "Basic Science Concepts".to_string(),
+            size: 312 * 1024 * 1024,
+            status: VideoStatus::Downloading(Progress(0.5)),
+        },
+        LocalVideoMeta {
+            id: "3".to_string(),
+            name: "English Grammar Fundamentals".to_string(),
+            size: 189 * 1024 * 1024,
+            status: VideoStatus::Downloaded,
+        },
+        LocalVideoMeta {
+            id: "4".to_string(),
+            name: "History of Ancient Civilizations".to_string(),
+            size: 456 * 1024 * 1024,
+            status: VideoStatus::Failed,
+        },
+        LocalVideoMeta {
+            id: "5".to_string(),
+            name: "Environmental Science Basics".to_string(),
+            size: 378 * 1024 * 1024,
+            status: VideoStatus::Downloaded,
+        },
+    ]
+});
+
+#[get("/content/meta")]
+async fn list_content_metadata(
+    web::Query(query): web::Query<vds_api::api::content::meta::get::Query>,
+) -> impl Responder {
+    use vds_api::api::content::meta::get::Response;
+
+    let response = {
+        let mut videos = MOCK_VIDEOS.clone();
+        if let Some(limit) = query.limit {
+            videos.resize_with(limit.min(videos.len()), || unreachable!());
+        }
+        Response { videos }
+    };
+    HttpResponse::Ok().json(response)
+}
+
+#[get("/content/meta/{id}")]
+async fn content_metadata_for_id(id: web::Path<String>) -> impl Responder {
+    use vds_api::api::content::meta::single::get::Response;
+
+    let response = Response {
+        meta: MOCK_VIDEOS.iter().find(|v| v.id == *id).cloned(),
+    };
+    HttpResponse::Ok().json(response)
+}
 
 async fn get_content_inner(content_path: &Path, id: &str) -> HttpResponse {
     let mut filepath = content_path.join(id);
@@ -25,18 +92,21 @@ async fn get_content_inner(content_path: &Path, id: &str) -> HttpResponse {
     HttpResponse::Ok().content_type("video/mp4").body(data)
 }
 
-#[get("/content")]
-async fn get_content(
-    content_path: web::Data<PathBuf>,
-    web::Query(query): web::Query<vds_api::api::content::get::Query>,
-) -> impl Responder {
-    get_content_inner(content_path.get_ref(), &query.id).await
+#[get("/content/{id}")]
+async fn get_content(content_path: web::Data<PathBuf>, id: web::Path<String>) -> impl Responder {
+    get_content_inner(content_path.get_ref(), &id).await
 }
 
-#[get("/content/{id}")]
-async fn get_content_path(
-    content_path: web::Data<PathBuf>,
-    id: web::Path<String>,
-) -> impl Responder {
-    get_content_inner(content_path.get_ref(), &id).await
+#[get("/manifest/latest")]
+async fn get_manifest() -> impl Responder {
+    let manifest_file = include_str!("../../../docs/manifest-example.json");
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(manifest_file)
+}
+
+#[post("/manifest/fetch")]
+async fn fetch_manifest() -> impl Responder {
+    // TODO
+    HttpResponse::Ok()
 }
