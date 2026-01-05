23b90e460545d94f8899c8d3cfed057c45116347
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-01-05 01:27:03 +0100

    Implement DB support

--- vds-server/build.rs
+++ vds-server/build.rs
@@ -1,6 +1,11 @@
 use static_files::resource_dir;
 
 fn main() -> std::io::Result<()> {
+    // Migrations are embedded in the server so that we can initialize a db from scratch
+    println!("cargo::rerun-if-changed=./migrations");
+
+    // The VDS site code is embedded so that we only need to run a single binary
+    // without additional dependencies.
     println!("cargo::rerun-if-changed=../vds-site/dist");
     resource_dir("../vds-site/dist").build()
 }
