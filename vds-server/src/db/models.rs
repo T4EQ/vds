14970f48cd38ce6ebe2504d88fd3a2846c5c1816
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-01-08 15:52:14 +0100

    Get rid of Diesel boilerplate
    - Remove `VideoInner` struct
    - Properly implement `Selectable` and `Queriable` for `Video` and
      `DownloadStatus`

--- vds-server/src/db/models.rs
+++ vds-server/src/db/models.rs
@@ -1,6 +1,9 @@
 use std::{ffi::OsString, os::unix::ffi::OsStringExt, path::PathBuf};
 
-use diesel::prelude::*;
+use diesel::{
+    prelude::*,
+    sql_types::{BigInt, Binary, Text},
+};
 
 use super::schema;
 
@@ -12,58 +15,89 @@ pub enum DownloadStatus {
     Downloaded(PathBuf),
 }
 
+impl Selectable<diesel::sqlite::Sqlite> for DownloadStatus {
+    type SelectExpression = (
+        schema::videos::dsl::file_size,
+        schema::videos::dsl::downloaded_size,
+        schema::videos::dsl::download_status,
+        schema::videos::dsl::message,
+        schema::videos::dsl::file_path,
+    );
+
+    fn construct_selection() -> Self::SelectExpression {
+        (
+            schema::videos::dsl::file_size,
+            schema::videos::dsl::downloaded_size,
+            schema::videos::dsl::download_status,
+            schema::videos::dsl::message,
+            schema::videos::dsl::file_path,
+        )
+    }
+}
+
+impl Queryable<(BigInt, BigInt, BigInt, Text, Binary), diesel::sqlite::Sqlite> for DownloadStatus {
+    type Row = (i64, i64, i64, String, Vec<u8>);
+
+    fn build(
+        (file_size, downloaded_size, download_status, message, file_path): Self::Row,
+    ) -> diesel::deserialize::Result<Self> {
+        Ok(match download_status {
+            DOWNLOAD_STATUS_NOT_STARTED => DownloadStatus::Pending,
+            DOWNLOAD_STATUS_FAILED => DownloadStatus::Failed(message),
+            DOWNLOAD_STATUS_IN_PROGRESS => {
+                DownloadStatus::InProgress((downloaded_size as u64, file_size as u64))
+            }
+            DOWNLOAD_STATUS_DOWNLOADED => {
+                DownloadStatus::Downloaded(OsString::from_vec(file_path).into())
+            }
+            v => {
+                return Err(super::Error::InvalidDownloadStatus(v).into());
+            }
+        })
+    }
+}
+
 pub const DOWNLOAD_STATUS_NOT_STARTED: i64 = 0;
 pub const DOWNLOAD_STATUS_FAILED: i64 = 1;
 pub const DOWNLOAD_STATUS_IN_PROGRESS: i64 = 2;
 pub const DOWNLOAD_STATUS_DOWNLOADED: i64 = 3;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Queryable, Debug, Clone, PartialEq, Eq)]
+#[diesel(table_name = schema::videos)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Video {
+    #[diesel(deserialize_as = String)]
     pub id: uuid::Uuid,
+
     pub name: String,
+
+    #[diesel(deserialize_as = i64)]
     pub file_size: u64,
+
     pub download_status: DownloadStatus,
+
+    #[diesel(deserialize_as = i64)]
     pub view_count: u64,
 }
 
-impl TryFrom<VideoInner> for Video {
-    type Error = super::Error;
-    fn try_from(value: VideoInner) -> Result<Self, super::Error> {
-        let download_status = match value.download_status {
-            DOWNLOAD_STATUS_NOT_STARTED => DownloadStatus::Pending,
-            DOWNLOAD_STATUS_FAILED => DownloadStatus::Failed(value.message),
-            DOWNLOAD_STATUS_IN_PROGRESS => {
-                DownloadStatus::InProgress((value.downloaded_size as u64, value.file_size as u64))
-            }
-            DOWNLOAD_STATUS_DOWNLOADED => {
-                DownloadStatus::Downloaded(OsString::from_vec(value.file_path).into())
-            }
-            v => {
-                return Err(super::Error::InvalidDownloadStatus(v));
-            }
-        };
-        Ok(Self {
-            id: value.id.try_into()?,
-            name: value.name,
-            file_size: value.file_size as u64,
-            download_status,
-            view_count: value.view_count as u64,
-        })
-    }
-}
+impl Selectable<diesel::sqlite::Sqlite> for Video {
+    type SelectExpression = (
+        schema::videos::dsl::id,
+        schema::videos::dsl::name,
+        schema::videos::dsl::file_size,
+        <DownloadStatus as Selectable<diesel::sqlite::Sqlite>>::SelectExpression,
+        schema::videos::dsl::view_count,
+    );
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = schema::videos)]
-#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct VideoInner {
-    pub id: String,
-    pub name: String,
-    pub file_size: i64,
-    pub downloaded_size: i64,
-    pub download_status: i64,
-    pub view_count: i64,
-    pub message: String,
-    pub file_path: Vec<u8>,
+    fn construct_selection() -> Self::SelectExpression {
+        (
+            schema::videos::dsl::id,
+            schema::videos::dsl::name,
+            schema::videos::dsl::file_size,
+            <DownloadStatus as Selectable<diesel::sqlite::Sqlite>>::construct_selection(),
+            schema::videos::dsl::view_count,
+        )
+    }
 }
 
 #[derive(Insertable)]
