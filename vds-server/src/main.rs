b3b2d757d715679caf2b647c7048458e05c4ea17
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-01-17 14:51:41 +0100

    Add additional metadata to cargo toml

--- vds-server/src/main.rs
+++ vds-server/src/main.rs
@@ -21,16 +21,8 @@ pub mod build_info {
     std::include!(std::concat!(std::env!("OUT_DIR"), "/built.rs"));
 
     pub fn print_version_info() {
-        println!("{PKG_NAME}: {PKG_VERSION}");
-        println!("\tAuthors: {PKG_AUTHORS}");
-        println!("\tHomepage: {PKG_HOMEPAGE}");
-        println!("\tLicense: {PKG_LICENSE}");
-        println!("\tRepository: {PKG_REPOSITORY}");
-
-        println!("Build info:");
-        println!("\tProfile: {PROFILE}");
-        println!("\trustc version: {RUSTC_VERSION}");
-        println!("\tFeatures: {FEATURES_STR}");
+        println!("{PKG_NAME}:");
+        println!("\tVersion: {PKG_VERSION}");
         if let Some(git_hash) = GIT_COMMIT_HASH {
             let dirty = if GIT_DIRTY.is_some_and(|v| v) {
                 "-dirty"
@@ -41,8 +33,21 @@ pub mod build_info {
         } else if let Some(hash) = std::option_env!("VDS_SERVER_NIX_GIT_REVISION") {
             println!("\tGit hash: {hash}");
         } else {
-            println!("\tGit hash: unknown");
+            println!("\tGit hash: Unknown");
         }
+        println!("\tAuthors:");
+        for author in PKG_AUTHORS.split(':') {
+            let author = author.trim();
+            println!("\t\t{author}");
+        }
+        println!("\tHomepage: {PKG_HOMEPAGE}");
+        println!("\tLicense: {PKG_LICENSE}");
+        println!("\tRepository: {PKG_REPOSITORY}");
+        println!();
+        println!("Build info:");
+        println!("\tProfile: {PROFILE}");
+        println!("\trustc version: {RUSTC_VERSION}");
+        println!("\tFeatures: {FEATURES_STR}");
     }
 }
 
