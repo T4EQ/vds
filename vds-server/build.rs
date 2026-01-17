3dcfe32a7b388f88b935f5729ef5d31f5b2bda07
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-01-17 13:50:25 +0100

    Add build information to the binary

--- vds-server/build.rs
+++ vds-server/build.rs
@@ -4,6 +4,9 @@ fn main() -> std::io::Result<()> {
     // Migrations are embedded in the server so that we can initialize a db from scratch
     println!("cargo::rerun-if-changed=./migrations");
 
+    // Record build-time information
+    built::write_built_file()?;
+
     let frontend_path = std::env::var("VDS_SERVER_FRONTEND_PATH")
         .unwrap_or_else(|_| "../vds-site/dist".to_string());
 
--- vds-server/src/main.rs
+++ vds-server/src/main.rs
@@ -7,15 +7,48 @@ struct Args {
     /// Path to the VDS configuration
     #[arg(short, long)]
     config: Option<PathBuf>,
+
+    /// Displays version information
+    #[arg(short, long)]
+    version: bool,
 }
 
 fn default_config_path() -> PathBuf {
     "/var/lib/vds/config/config.toml".into()
 }
 
+pub mod build_info {
+    std::include!(std::concat!(std::env!("OUT_DIR"), "/built.rs"));
+
+    pub fn print_version_info() {
+        println!("{PKG_NAME}: {PKG_VERSION}");
+        println!("\tAuthors: {PKG_AUTHORS}");
+        println!("\tHomepage: {PKG_HOMEPAGE}");
+        println!("\tLicense: {PKG_LICENSE}");
+        println!("\tRepository: {PKG_REPOSITORY}");
+
+        println!("Build info:");
+        println!("\tProfile: {PROFILE}");
+        println!("\trustc version: {RUSTC_VERSION}");
+        println!("\tFeatures: {FEATURES_STR}");
+        if let Some(git_hash) = GIT_COMMIT_HASH {
+            let dirty = if GIT_DIRTY.is_some_and(|v| v) {
+                "-dirty"
+            } else {
+                ""
+            };
+            println!("\tGit hash: {git_hash}{dirty}");
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    if args.version {
+        build_info::print_version_info();
+        return Ok(());
+    }
     let config = vds_server::cfg::get_config(&args.config.unwrap_or_else(default_config_path))?;
 
     let listener = TcpListener::bind(format!(
