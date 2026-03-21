83927203de35e7cf9e757eb37ae57e87ec56a01b
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-03-21 20:50:28 +0100

    Modularize nix configuration

---
+++ leap-server/default.nix
@@ -0,0 +1,50 @@
+top@{ inputs, ... }:
+{
+  perSystem =
+    { config, pkgs, ... }:
+    let
+      # Note that these pacakges do not obey the rust-toolchain.toml. Unfortunately, building the
+      # toolchains from the rust-toolchain.toml files would take way too long. Maybe we could
+      # do this if we setup a nix cache, but for now this will do.
+      leapPackageWithPkgs =
+        pkgs: targetPkgs:
+        targetPkgs.rustPlatform.buildRustPackage {
+          pname = "leap-server";
+          version = "0.1.0";
+          src = inputs.self;
+
+          cargoLock = {
+            lockFile = ./../Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            lld
+          ];
+
+          buildPhase = "
+            runHook preBuild
+            pushd leap-server
+            export LEAP_SERVER_FRONTEND_PATH=${config.packages.leap-site}/dist
+            export CARGO_TARGET_DIR=$(pwd)/../target
+            export LEAP_SERVER_NIX_GIT_REVISION=${top.config.git-rev}
+            cargo build --release --offline -j $NIX_BUILD_CORES --target ${targetPkgs.stdenv.hostPlatform.rust.rustcTarget} --no-default-features
+            popd
+            runHook postBuild
+          ";
+        };
+
+      targetPkgs = import inputs.nixpkgs {
+        inherit (pkgs) system;
+        crossSystem = {
+          isStatic = true;
+          config = "aarch64-unknown-linux-musl";
+        };
+      };
+    in
+    {
+      packages = {
+        leap = leapPackageWithPkgs pkgs pkgs;
+        leap-target = leapPackageWithPkgs pkgs targetPkgs;
+      };
+    };
+}
---
+++ leap-site/default.nix
@@ -0,0 +1,46 @@
+{ inputs, ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages = {
+        # The site is always cross compiled using trunk and wasm. That's why we can build it with the
+        # host toolchain, which already has support for wasm. However, the same is not true for
+        # the target toolchain (aarch64-unknown-linux-musl), so we need to use a nix cross-compilation
+        # strategy there
+        leap-site = pkgs.rustPlatform.buildRustPackage {
+          pname = "leap-site";
+          version = "0.1.0";
+          src = inputs.self;
+
+          cargoLock = {
+            lockFile = ./../Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            trunk
+            wasm-bindgen-cli_0_2_106
+            dart-sass
+            lld
+          ];
+
+          buildPhase = "
+              runHook preBuild
+              pushd leap-site
+              trunk build --release --offline
+              popd
+              runHook postBuild
+            ";
+
+          installPhase = "
+              runHook preInstall
+              pushd leap-site
+              mkdir -p $out
+              cp -r dist $out/
+              popd
+              runHook postInstall
+            ";
+        };
+      };
+    };
+}
